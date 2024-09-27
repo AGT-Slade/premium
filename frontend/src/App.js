5a88d9e32609179fb7fa41062571ee615f3c4129
AGT-Slade <josendez17@gmail.com>
2024-09-27 17:54:32 -0600

    Video-05-Create-List-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import data from './data.js';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/" target="" rel="">Premium</a>
+        <a href="/" target="" rel=""><h1>Premium</h1></a>
       </header>
       <main>
-        list products
+        <h1>Products</h1>
+        <div className="products">
+        {data.products.map(
+          function (productArray) {
+            return (
+              <div className="product" key={productArray.slug}>
+                <a href={`/product/${productArray.slug}`}>
+                  <img src={productArray.image} alt={productArray.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${productArray.slug}`}>
+                    <p>{productArray.name}</p>
+                  </a>
+                  <p><strong>{productArray.price}</strong></p>
+                  <button type="button">Add to cart</button>
+                </div>
+              </div>
+            )
+          }
+        )}
+        </div>
       </main>
     </div>
   );
