b366f326c081094282263f67ef755b5d93dd3f08
AGT-Slade <josendez17@gmail.com>
2024-10-08 18:26:58 -0600

    Video-22-Create-Signin-Backend-API

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import data from './data.js';
+//import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import cors from 'cors'; 
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,11 +18,17 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 
 const app = express();
-
 app.use(cors());
+
+
+app.use(express.json());   
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
 
 
 app.get('/', (req, res) => {
@@ -30,14 +37,22 @@ app.get('/', (req, res) => {
 
 
 
+// app.use('/api/GETtest', (req, res) => {
+//     const heading = `<h1>Hello World</h1>`;
+//     res.send(heading);
+// });
 
-app.get('/api/test', (req, res) => {
-    const heading = `<h1>Hello World</h1>`;
-    res.send(heading);
+// app.use('/api/users', userRouter);
+
+
+
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });    
 });
 
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.listen(port, (req, res) => {
     console.log(`Server at http://localhost:${port}`);
 });
